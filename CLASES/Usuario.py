a4ea042c164e7569fffe65e86e614d5cc2b55f31
cristianquiroz6211 <Cristian.quiroz6211@uco.net.co>
2023-09-04 15:11:07 -0500

    Se agrega clase Usuario con atributos

---
+++ CLASES/Usuario.py
@@ -0,0 +1,17 @@
+class Usuario:
+    """
+    Attributos:
+        id (int): Identificador del usuario.
+        nombre (str): Nombre del usuario.
+        apellido (str): Apellido del usuario.
+        numerocontacto (str): Número de contacto del usuario.
+        RolUsuario (Rol): Rol del usuario.
+    """
+
+    def __init__(self, id, nombre, apellido, numerocontacto, RolUsuario):
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellido
+        self.numerocontacto = numerocontacto
+        self.RolUsuario = RolUsuario
+
