cfbd06f0b6e6635fa195cfb97168ee524eda4b9c
Daniel Puerta <daniel.puerta0522@uco.net.co>
2023-11-06 18:20:24 -0500

    ui

--- Programa/views/Usuarios/AdministradorGeneral_ui.py
+++ Programa/views/Usuarios/AdministradorGeneral_ui.py
@@ -5,11 +5,16 @@ class AdministradorGeneralView(QtWidgets.QWidget):
         super().__init__()
         self.setWindowTitle("Gestión de Usuarios")
         self.setGeometry(100, 100, 800, 600)
-        self.layout = QtWidgets.QVBoxLayout()
+        
+        # Layout principal en forma de QHBoxLayout
+        self.layout = QtWidgets.QHBoxLayout()
         self.setLayout(self.layout)
         
+        # Menú lateral en forma de QVBoxLayout
+        menu_layout = QtWidgets.QVBoxLayout()
+        
         self.label = QtWidgets.QLabel("Bienvenido")
-        self.layout.addWidget(self.label)
+        menu_layout.addWidget(self.label)
         
         # Botones para diferentes roles
         self.btn_inicio = QtWidgets.QPushButton("Inicio")
@@ -19,12 +24,12 @@ class AdministradorGeneralView(QtWidgets.QWidget):
         self.btn_estadisticas = QtWidgets.QPushButton("Estadísticas")
         self.btn_logout = QtWidgets.QPushButton("Cerrar Sesión")
         
-        self.layout.addWidget(self.btn_inicio)
-        self.layout.addWidget(self.btn_locales)
-        self.layout.addWidget(self.btn_usuarios)
-        self.layout.addWidget(self.btn_productos)
-        self.layout.addWidget(self.btn_estadisticas)
-        self.layout.addWidget(self.btn_logout)
+        menu_layout.addWidget(self.btn_inicio)
+        menu_layout.addWidget(self.btn_locales)
+        menu_layout.addWidget(self.btn_usuarios)
+        menu_layout.addWidget(self.btn_productos)
+        menu_layout.addWidget(self.btn_estadisticas)
+        menu_layout.addWidget(self.btn_logout)
         
         # Conectar las señales de clic de los botones a las funciones correspondientes
         self.btn_inicio.clicked.connect(self.mostrar_inicio)
@@ -33,6 +38,9 @@ class AdministradorGeneralView(QtWidgets.QWidget):
         self.btn_productos.clicked.connect(self.mostrar_productos)
         self.btn_estadisticas.clicked.connect(self.mostrar_estadisticas)
         self.btn_logout.clicked.connect(self.cerrar_sesion)
+        
+        # Agregar el menú lateral al layout principal
+        self.layout.addLayout(menu_layout)
         
         # Widget para mostrar contenido dinámico
         self.contenido = QtWidgets.QWidget()
@@ -43,40 +51,40 @@ class AdministradorGeneralView(QtWidgets.QWidget):
         # Puedes crear un nuevo widget o mostrar el contenido que desees en la vista de "Inicio"
         # Por ejemplo, aquí se muestra una etiqueta con el texto "Página de Inicio":
         inicio_content = QtWidgets.QLabel("Página de Inicio")
-        self.layout.addWidget(inicio_content)
         self.contenido = inicio_content
+        self.layout.replaceWidget(self.layout.itemAt(1).widget(), inicio_content)
     
     def mostrar_locales(self):
         self.contenido.hide()
         # Aquí puedes mostrar el contenido relacionado con "Locales"
         # Por ejemplo, una etiqueta con el texto "Gestión de Locales":
         locales_content = QtWidgets.QLabel("Gestión de Locales")
-        self.layout.addWidget(locales_content)
         self.contenido = locales_content
+        self.layout.replaceWidget(self.layout.itemAt(1).widget(), locales_content)
     
     def mostrar_usuarios(self):
         self.contenido.hide()
         # Aquí puedes mostrar el contenido relacionado con "Usuarios"
         # Por ejemplo, una etiqueta con el texto "Gestión de Usuarios":
         usuarios_content = QtWidgets.QLabel("Gestión de Usuarios")
-        self.layout.addWidget(usuarios_content)
         self.contenido = usuarios_content
+        self.layout.replaceWidget(self.layout.itemAt(1).widget(), usuarios_content)
     
     def mostrar_productos(self):
         self.contenido.hide()
         # Aquí puedes mostrar el contenido relacionado con "Productos"
         # Por ejemplo, una etiqueta con el texto "Gestión de Productos":
         productos_content = QtWidgets.QLabel("Gestión de Productos")
-        self.layout.addWidget(productos_content)
         self.contenido = productos_content
+        self.layout.replaceWidget(self.layout.itemAt(1).widget(), productos_content)
     
     def mostrar_estadisticas(self):
         self.contenido.hide()
         # Aquí puedes mostrar el contenido relacionado con "Estadísticas"
         # Por ejemplo, una etiqueta con el texto "Estadísticas de la Administración":
         estadisticas_content = QtWidgets.QLabel("Estadísticas de la Administración")
-        self.layout.addWidget(estadisticas_content)
         self.contenido = estadisticas_content
+        self.layout.replaceWidget(self.layout.itemAt(1).widget(), estadisticas_content)
     
     def cerrar_sesion(self):
         self.close()
@@ -87,5 +95,3 @@ if __name__ == "__main__":
     ventana = AdministradorGeneralView()
     ventana.show()
     sys.exit(app.exec_())
-
-
---
+++ Programa/views/Usuarios/Cocinero_ui.py
@@ -0,0 +1,51 @@
+#Crear cocinero con un menu lataral con las opciones de: inicio,notificaciones, cerrar sesion
+
+from PyQt5 import QtWidgets
+
+class CocineroView(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Gestión de Usuarios")
+        self.setGeometry(100, 100, 800, 600)
+        self.layout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.layout)
+        
+        self.label = QtWidgets.QLabel("Bienvenido")
+        self.layout.addWidget(self.label)
+        
+        # Botones para diferentes roles
+        self.btn_inicio = QtWidgets.QPushButton("Inicio")
+        self.btn_notificaciones = QtWidgets.QPushButton("Notificaciones")
+        self.btn_logout = QtWidgets.QPushButton("Cerrar Sesión")
+        
+        self.layout.addWidget(self.btn_inicio)
+        self.layout.addWidget(self.btn_notificaciones)
+        self.layout.addWidget(self.btn_logout)
+
+        # Conectar las señales de clic de los botones a las funciones correspondientes
+        self.btn_inicio.clicked.connect(self.mostrar_inicio)
+        self.btn_notificaciones.clicked.connect(self.mostrar_notificaciones)
+        self.btn_logout.clicked.connect(self.cerrar_sesion)
+        
+        # Widget para mostrar contenido dinámico
+        self.contenido = QtWidgets.QWidget()
+        self.layout.addWidget(self.contenido)
+        
+    def mostrar_inicio(self):
+        self.contenido.hide()
+        # Puedes crear un nuevo widget o mostrar el contenido que desees en la vista de "Inicio"
+        # Por ejemplo, aquí se muestra una etiqueta con el texto "Página de Inicio":
+        inicio_content = QtWidgets.QLabel("Página de Inicio")
+        self.layout.addWidget(inicio_content)
+        self.contenido = inicio_content
+    
+    def mostrar_notificaciones(self):
+        self.contenido.hide()
+        # Aquí puedes mostrar el contenido relacionado con "Locales"
+        # Por ejemplo, una etiqueta con el texto "Gestión de Locales":
+        notificaciones_content = QtWidgets.QLabel("Notificaciones")
+        self.layout.addWidget(notificaciones_content)
+        self.contenido = notificaciones_content
+    
+    def cerrar_sesion(self):
+        self.close()
